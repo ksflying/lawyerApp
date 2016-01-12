3ad82aa3c75b26b105c396bedf2ea6c769ca33b3
[lith] <[lith@alongdata.com]>
2016-01-12 10:24:56 +0800

    Initial Commit

---
+++ lawyerApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lawyerApp
+//
+//  Created by mdhj－2 on 16/1/12.
+//  Copyright © 2016年 mdhj－2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
